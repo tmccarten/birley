97497ffddb21ef10c5edd930eecb0df1b1e9c792
tmccarten <thomasmccarten@hotmail.com>
2017-06-27 17:35:40 +0100

    initial

---
+++ assets/includes/head.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title><?php echo $pageTitle; ?></title>
+
+  <meta name="description" content="<?php echo $pageDescription; ?>">
+  <meta name="keywords" content="<?php echo $pageKeywords; ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="../css/normalize.css">
+  <link rel="stylesheet" href="../css/styles.css">
+
+  <link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
+  <link rel="icon" href="../../favicon.ico" type="image/x-icon">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+  <p class="hide-skip"> <!-- Skip links -->
+    <a href="#skipnav" title="Skip to the content">Skip to content</a>
+    <a href="#skiptonav" title="Skip to the navigation">Skip to navigation</a>
+  </p>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$pageTitle = "Home | Living Lines: Birley Archives";
+$pageDescription = "Archives, history and information on the Birley area of Hulme, Manchester.";
+$pageKeywords = "Birley, History, Archives";
+$activePage = "Home";
+
+include ('assets/includes/head.php');
+?>
+
+</body>
+</html>
