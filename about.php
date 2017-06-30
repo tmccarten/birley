2bf976113953dfa88ec761cd040742906ce0f45b
tmccarten <thomasmccarten@hotmail.com>
2017-06-30 18:19:53 +0100

    about, contact and cursor files

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php
+
+$pageTitle = "About | Living Lines: Birley Archives";
+$pageDescription = "About the Living Lines Birley project.";
+$pageKeywords = "Birley, About";
+$activePage = "About";
+
+$css = "assets/css/";
+
+$favIcon = "favicon.ico";
+
+include('assets/includes/head.php');?>
+
+</head>
+
+<?php
+
+$linkHome = "index.php";
+$linkAbout = ".";
+$linkContact = "contact.php";
+
+$linkHealth = "birley-health-and-education/";
+$linkArchive = "birley-archive/";
+$linkGrow = "grow-birley/";
+$linkMy = "my-birley/";
+
+include('assets/includes/header.php'); ?>
+
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+
+$pageTitle = "Contact | Living Lines: Birley Archives";
+$pageDescription = "Contact details for the Living Lines Birley Art Project.";
+$pageKeywords = "Birley, Contact";
+$activePage = "Contact";
+
+$css = "assets/css/";
+
+$favIcon = "favicon.ico";
+
+include('assets/includes/head.php');?>
+
+</head>
+
+<?php
+
+$linkHome = "index.php";
+$linkAbout = "about.php";
+$linkContact = ".";
+
+$linkHealth = "birley-health-and-education/";
+$linkArchive = "birley-archive/";
+$linkGrow = "grow-birley/";
+$linkMy = "my-birley/";
+
+include('assets/includes/header.php'); ?>
+
+</body>
+</html>
