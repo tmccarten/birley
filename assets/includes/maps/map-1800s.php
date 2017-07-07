88401d58c506db759b74a2849585c3ef5b78ec44
tmccarten <thomasmccarten@hotmail.com>
2017-07-07 13:07:33 +0100

    further icons and hover

--- assets/includes/maps/map-1800s.php
+++ assets/includes/maps/map-1800s.php
@@ -1,10 +1,17 @@
 <div class="map__inner map__inner--1800s">
 
+  <div class="icon icon__church">
+    <div class="icon__church--hover">
+      <p>A carved stone head, possibly representing church benefactress Eleanor Atherton, was recovered from demolition material at the site of Holy Trinity Church (built in 1843) along with pieces of church glazing and other artefacts. The church was damaged by an air raid in WWII and subsequently demolished in 1953.</p>
+      <p>There was also a Catholic Apostolic church dating from around the 1840’s.</p>
+    </div>
+  </div>
+
   <div class="icon icon__terraces">
     <div class="icon__terraces--hover">
       <p>The biggest development of workers’ houses in the industrial era was from the mid to late 1800s. The site and its surroundings formed a grid like pattern of streets, much of which was
       terraced housing. When built, the houses had small individual rear yards with ‘privies’: outdoor toilets.</p>
-      <p>The terraces were cleared for a programme of new housing development in the 1960s.</p>      
+      <p>The terraces were cleared for a programme of new housing development in the 1960s.</p>
     </div>
   </div>
 
--- assets/includes/maps/map-pre1800s.php
+++ assets/includes/maps/map-pre1800s.php
@@ -1,4 +1,10 @@
 <div class="map__inner map__inner--pre">
 
+  <div class="icon icon__farm">
+    <div class="icon__farm--hover">
+      <p>The site of Jackson’s Farm lies within the present Birley boundaries, and is shown on an OS map of 1848 when it comprised a farmhouse and two further buildings to the north. It is not known when the farm was first established, but potentially the site might be of medieval origin.</p>
+    </div>
+  </div>
+
 
 </div>
