f3d96e81afea179a7fcc5ffdde880a19d9749a48
tmccarten <thomasmccarten@hotmail.com>
2017-07-07 04:23:52 +0100

    further icon/hovers

--- assets/includes/maps/map-1800s.php
+++ assets/includes/maps/map-1800s.php
@@ -1,4 +1,12 @@
 <div class="map__inner map__inner--1800s">
 
+  <div class="icon icon__terraces">
+    <div class="icon__terraces--hover">
+      <p>The biggest development of workers’ houses in the industrial era was from the mid to late 1800s. The site and its surroundings formed a grid like pattern of streets, much of which was
+      terraced housing. When built, the houses had small individual rear yards with ‘privies’: outdoor toilets.</p>
+      <p>The terraces were cleared for a programme of new housing development in the 1960s.</p>      
+    </div>
+  </div>
+
 
 </div>
