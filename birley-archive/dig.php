5354c315bbf230ae1d95a73fa1e926673b27f6a8
tmccarten <thomasmccarten@hotmail.com>
2017-07-10 19:35:45 +0100

    page text

--- birley-archive/dig.php
+++ birley-archive/dig.php
@@ -35,11 +35,16 @@ include('../assets/includes/header.php'); ?>
 
   <div class="main-icon">
     <img src="<?php echo $assets?>/img/maps/icons/icon_bust.svg" class="main-icon__img" alt="Illustration of carved stone church bust">
-    <h2 class="main-icon__text">Archaeological Dig</h2> 
+    <h2 class="main-icon__text">Archaeological Dig</h2>
   </div>
 
   <section class="main-text">
     <h2 class="main-text__title">Archaeological Dig</h2>
+
+    <p>Between 2011 and 2012, the university commissioned an archaeological investigation on the site that was to become the Birley development.</p>
+    <p>The <a href="#" class="main-text__link">fascinating report</a> reveals the layers of history that lie beneath the site of the university campus; its alternative foundations that take the site through agricultural land, industrial landscapes, beautiful churches and urban regeneration.</p>
+    <p>In March 2013, an exhibition was held of the finds. Further information can be found at the University’s <a href="#" class="main-text__link">Special Collections</a>, based on the third floor of the Library at the All Saints Campus.</p>
+
   </section>
 
 
