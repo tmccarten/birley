643eb9d5d3143b403e94e941d01c17affa5eb341
tmccarten <thomasmccarten@hotmail.com>
2017-07-11 02:45:33 +0100

    page structure

--- birley-archive/dig.php
+++ birley-archive/dig.php
@@ -34,8 +34,7 @@ include('../assets/includes/header.php'); ?>
 <main id="skipnav" class="main">
 
   <div class="main-icon">
-    <img src="<?php echo $assets?>/img/maps/icons/icon_bust.svg" class="main-icon__img" alt="Illustration of carved stone church bust">
-    <h2 class="main-icon__text">Archaeological Dig</h2>
+    <img src="<?php echo $assets?>/img/maps/icons/icon_bust.svg" class="main-icon__img" alt="Illustration of carved stone church bust">    
   </div>
 
   <section class="main-text">
